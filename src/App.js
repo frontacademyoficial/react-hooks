c152cb22ef01d0420531e05f69b272eaf582585a
Bruno Lucena <bruno.lucenac@gmail.com>
2023-02-08 17:00:39 -0300

    React Avançado - useState - Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Counter from "./react-hooks/Counter";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          margin: "1rem",
+        }}
+      >
+        <h1 style={{ marginBottom: "2rem" }}>React Avançado - useState</h1>
+
+        <Counter />
+      </div>
     </div>
   );
 }
-
-export default App;
